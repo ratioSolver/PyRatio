575d00a6c8554f03f258449f571d28f6d539a91c
riccardodebenedictis <riccardo.debenedictis@istc.cnr.it>
2022-09-11 15:27:07 +0200

    minor cleanings..

--- src/py_solver_listener.cpp
+++ src/py_solver_listener.cpp
@@ -23,7 +23,7 @@ namespace ratio::python
         // the flaw's current estimated cost..
         const auto est_cost = f.get_estimated_cost();
 
-        py_slv.attr("fire_flaw_cost_changed")(get_id(f), slv.get_sat_core()->value(f.get_phi()), est_cost.numerator(), est_cost.denominator());
+        py_slv.attr("fire_flaw_cost_changed")(get_id(f), pybind11::module_::import("oRatio.rational").attr("Rational")(est_cost.numerator(), est_cost.denominator()));
     }
     void py_solver_listener::flaw_position_changed(const ratio::solver::flaw &f)
     {
@@ -39,7 +39,7 @@ namespace ratio::python
         // the resolver's intrinsic cost..
         const auto intr_cost = r.get_intrinsic_cost();
 
-        py_slv.attr("fire_resolver_created")(get_id(r), get_id(r.get_effect()), r.get_data().dump(), intr_cost.numerator(), intr_cost.denominator(), slv.get_sat_core()->value(r.get_rho()));
+        py_slv.attr("fire_resolver_created")(get_id(r), get_id(r.get_effect()), r.get_data().dump(), pybind11::module_::import("oRatio.rational").attr("Rational")(intr_cost.numerator(), intr_cost.denominator()), slv.get_sat_core()->value(r.get_rho()));
     }
     void py_solver_listener::resolver_state_changed(const ratio::solver::resolver &r) { py_slv.attr("fire_resolver_state_changed")(get_id(r), slv.get_sat_core()->value(r.get_rho())); }
     void py_solver_listener::current_resolver(const ratio::solver::resolver &r) { py_slv.attr("fire_current_resolver")(get_id(r)); }
