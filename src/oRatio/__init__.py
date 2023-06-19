c1e059cc752c5d46a80cdefa2c82129278095290
Riccardo De Benedictis <riccardo.debenedictis@istc.cnr.it>
2023-06-19 17:53:01 +0200

    bumped PyRatio version..

--- src/oRatio/__init__.py
+++ src/oRatio/__init__.py
@@ -1,5 +1,7 @@
+from oRatio.rational import Rational, InfRational
+from oRatio.item import Item, Atom
 from oRatio.solver import Solver
-from oRatio.executor import Executor
+from oRatio.executor import Executor, ExecutorState
 from oRatio.core_listener import CoreListener
 from oRatio.solver_listener import SolverListener
 from oRatio.executor_listener import ExecutorListener
